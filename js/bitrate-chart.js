1dd6fbee8e66ded4a683d471823b747af5305e36
Abdul Wahab <awahab@vodworks.com>
2015-11-02 20:45:54 +0500

    bar aligned

--- js/bitrate-chart.js
+++ js/bitrate-chart.js
@@ -1,37 +1,47 @@
 (function($){
-	var reading = [0, 1, 0, 3, 4, 5];
+	var reading = [2, 4, 5, 4, 0, 1, 0, 3, 4, 5];
 
 	$('#svg-chart').svg({onLoad: drawChart});
 
 	function drawChart(svg) {
-		var width = 600,
-			height = 400,
+		var width = 400,
+			height = 200,
 			nLevels = 7,
 			nCols = 10,
 			lineSize = 6,
 			stepWidth = width / nCols,
-			gapHeight = (height - (nLevels * lineSize)) / (nLevels - 1);
-
-		//svg.circle(130, 75, 50, {fill: 'none', stroke: 'red', strokeWidth: 3});
+			gapHeight = (height - (nLevels * lineSize)) / (nLevels - 1),
+			backgroundBarStyle = '#9A9997';
 		
 		// Box border
 		svg.rect(null, 0, 0, width, height, {fill: 'none', stroke: 'red'});
-
 		
+		// Background bars
+		for(var i=0; i<nLevels; i++) {
+			svg.rect(0, (i * gapHeight) + (i * lineSize), width, lineSize, {fill:backgroundBarStyle});
+		}
+
+		reading = [reading[0]].concat(reading);
+
 		reading.reduceRight(function(preVal, currVal, index, arr) {
-			//  Vertical Line
+			
 			var rIndex = reading.length - index - 1,
 				i = nCols - rIndex,
-				x = width - (rIndex * stepWidth),
-				w = lineSize,
-				y = preVal * gapHeight,
-				h = Math.abs(currVal - preVal) * gapHeight;
+				xRight = width - (rIndex * stepWidth),
+				xLeft = width - (rIndex * stepWidth) - stepWidth,
+				y = (preVal * gapHeight) + (preVal * lineSize),
+				yNext = (currVal * gapHeight) + (currVal * lineSize),
+				yVTop = Math.min(y, yNext),
+				yVBottom = Math.max(y, yNext),
+				h = currVal > preVal ? yVBottom - yVTop + lineSize: yVBottom - yVTop;
 			
-			console.log(i, stepWidth, x, y, w, h);
-			
-			svg.rect(null, x, y, w, h);
+			// Horizontal Line of previous step level
+			svg.rect(null, xRight, y, xRight - xLeft, lineSize);
+
+			// Vertical Line to previous step level from current step
+			svg.rect(null, xRight, yVTop, lineSize, h);
 
 			return currVal;
-		}, reading[reading.length - 1]);
+		});
 	}
 }(jQuery));
